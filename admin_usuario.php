276b9c5175e5912c67b52f15d28bf39685fdfc0e
ChavesArt <116437988+ChavesArt@users.noreply.github.com>
2024-08-09 17:05:22 -0300

    fiz o filtro do admin

--- admin_usuario.php
+++ admin_usuario.php
@@ -8,64 +8,28 @@ $conexao = conectar();
 logar();
 $logado = $_SESSION['nome'];
 
-if (empty($_GET['tabela'])) {
-    $sql_usuario = "SELECT * FROM usuario ORDER BY nome DESC";
-    $resultado_tabela_usuario =  mysqli_query($conexao, $sql_usuario);
+/*barra de pesquisa*/
+
+    $sql_pesquisar = "SELECT * FROM usuario ORDER BY nome DESC";
+
+if (!empty($_GET['pesquisar'])) {
+
+    $data = $_GET['pesquisar'];
+
+    $sql_pesquisar = $sql_pesquisar . " order by nome DESC";
 }
 
+if (isset($_GET['filtro'])) {
+    $filtro = $_GET['filtro'];   // todos  voluntarios
 
-
-    /*barra de pesquisa*/
-
-    // EM PROGRESSO
-    if (!empty($_GET['pesquisar'])) {
-
-        $data = $_GET['pesquisar'];
-       
-        $filtro =  "todos";      // $_GET['filtro'];   // todos  voluntarios
-
-        $sql_pesquisar = "SELECT * FROM usuario where (nome LIKE '%$data%' or endereco LIKE '%$data%' or email LIKE '%$data%' or  telefone LIKE '%$data%' )"; 
-
-
-        if ($filtro == 'voluntarios') { 
-            $sql_pesquisar    = $sql_pesquisar . " and ( voluntario  = true ) ";
-        }
-
-        
-
-        $sql_pesquisar = $sql_pesquisar . " order by nome DESC";
-
-      ///  if (empty($_GET['tabela'])) {
-           // $sql_pesquisar = "SELECT * FROM usuario where nome LIKE '%$data%' or endereco LIKE '%$data%' or email LIKE '%$data%' or  telefone LIKE '%$data%' order by nome DESC";
-         
-      //  }
+    $sql_pesquisar = "SELECT * FROM usuario ";
+    if ($filtro == 'voluntarios') {
+        $sql_pesquisar    = $sql_pesquisar . " Where voluntario = true  ";
     }
-     else { 
-        $sql_pesquisar = "SELECT * FROM usuario order by nome DESC";
-      //  $resultado_usuario = mysqli_query($conexao, $sql_usuario);
-    }
+    $sql_pesquisar .= " order by nome DESC";
+}
 
-    $resultado_pesquisar = mysqli_query($conexao, $sql_pesquisar);
-
-    //$res = mysqli_affected_rows($conexao);
-
-
-    // head-table
-
-/*
-    // SELECIONA OS USUARIOS
-    if (!empty($_GET['tabela'])) {
-        if ($_GET['tabela'] == 'usuário') {
-            $sql_usuario = "SELECT * FROM usuario ORDER BY nome DESC";
-            $resultado_tabela_usuario =  mysqli_query($conexao, $sql_usuario);
-        }
-        // SELECIONA OS VOLUNTÁRIOS                                     
-        if ($_GET['tabela'] == 'voluntário') {
-            $sql_usuario_voluntario = "SELECT * FROM usuario WHERE voluntario = 1";
-            $resultado_tabela_voluntario =  mysqli_query($conexao, $sql_usuario_voluntario);
-        }
-    }*/
-
+$resultado_pesquisar = mysqli_query($conexao, $sql_pesquisar);
 ?>
 
 <!DOCTYPE html>
@@ -87,7 +51,7 @@ if (empty($_GET['tabela'])) {
     <br><br>
     <br><br>
     <center>
-    <h4>Olá, <?php echo $logado; ?></h4>
+        <h4>Olá, <?php echo $logado; ?></h4>
         <h1>Usuários</h1>
     </center>
 
@@ -108,10 +72,15 @@ if (empty($_GET['tabela'])) {
     <center>
 
         <!-- RADIO -->
-        <input type="radio" id="todos" name="filtro" value="todos">
-        <label for="todos">Todos</label>
-        <input type="radio" id="voluntario" name="filtro" value="voluntarios">
-        <label for="voluntario">Voluntário</label><br>  
+        <form action="" method="GET">
+
+            <input type="radio" id="todos" name="filtro" value="todos">
+            <label for="todos">Todos</label>
+            <input type="radio" id="voluntario" name="filtro" value="voluntarios">
+            <label for="voluntario">Voluntário</label><br>
+
+            <button>Enviar</button>
+        </form>
     </center>
 
     <div class=" container p-5 ">
@@ -125,13 +94,9 @@ if (empty($_GET['tabela'])) {
                     </div>
 
                     <div class="btn-group">
-                        <form action="" method="get">
-                            <div class="btn btn-primary dropdown-toggle ">
-                                <button type="button" class="btn btn-primary" data-bs-toggle="dropdown">Usuários </button>
-                                <div class="dropdown-menu">
-                                    <input type="submit" class="dropdown-item" name="tabela" value="usuário">
-                                    <input type="submit" class="dropdown-item" name="tabela" value="voluntário">
-                                </div>
+                        <form action="#" method="get">
+                            <div class="btn btn-primary">
+                                <button type="button" class="btn btn-primary">Usuários</button>
                             </div>
                         </form>
                         <div class="btn-group">
@@ -151,29 +116,29 @@ if (empty($_GET['tabela'])) {
 
                 <?php
                 // HEAD do Usuário e Voluntário
- 
-                    echo "<tr>";
-                    echo "<th scope='col'>Nome</th>";
-                    echo "<th scope='col'>Endereço</th>";
-                    echo "<th scope='col'>Email</th>";
-                    echo "<th scope='col'>telefone</th>";
-                    echo "<th scope='col'>Opções</th>";
-                    echo "</tr>";
+
+                echo "<tr>";
+                echo "<th scope='col'>Nome</th>";
+                echo "<th scope='col'>Endereço</th>";
+                echo "<th scope='col'>Email</th>";
+                echo "<th scope='col'>telefone</th>";
+                echo "<th scope='col'>Opções</th>";
+                echo "</tr>";
                 ?>
             </thead>
             <tbody>
                 <?php
 
-              //  if (empty($_GET)) {
-                    // tabela com as informaçoes do banco sobre os usuários
-                    while ($info = mysqli_fetch_assoc($resultado_pesquisar)) {
+                //  if (empty($_GET)) {
+                // tabela com as informaçoes do banco sobre os usuários
+                while ($info = mysqli_fetch_assoc($resultado_pesquisar)) {
 
-                        echo '<tr>';
-                        echo '<td>' . $info['nome'] . '</td>';
-                        echo '<td>' . $info['endereco'] . '</td>';
-                        echo '<td>' . $info['email'] . '</td>';
-                        echo '<td>' . $info['telefone'] . '</td>';
-                        echo '<td>
+                    echo '<tr>';
+                    echo '<td>' . $info['nome'] . '</td>';
+                    echo '<td>' . $info['endereco'] . '</td>';
+                    echo '<td>' . $info['email'] . '</td>';
+                    echo '<td>' . $info['telefone'] . '</td>';
+                    echo '<td>
                         
                         <a class = "btn btn-sm btn-primary" href="crud/form-alterar.php?id_usuario=' . $info['id_usuario'] . '">
                         
@@ -190,14 +155,14 @@ if (empty($_GET['tabela'])) {
                         </a>
                         </td>';
 
-                        echo '</tr>';
-                    }
-              //  }
+                    echo '</tr>';
+                }
+                //  }
 
-             
-                    
-              
-            
+
+
+
+
                 ?>
 
             </tbody>
@@ -222,6 +187,37 @@ if (empty($_GET['tabela'])) {
                 });
             });
         });
+        // escuta
+
+        window.onload = function() {
+            document.getElementById('todos').addEventListener('click', envia_form);
+        }
+
+
+        function envia_form(id_form) {
+            let frm = $('#' + id_form);
+
+            frm.submit(function(e) {
+
+                e.preventDefault();
+
+                // submissão do formulário Ajax
+
+                $.ajax({
+                    type: frm.attr('method'),
+                    url: frm.attr('action') + '?filtro',
+                    data: frm.serialize(),
+                    // sucess
+                    sucess: function(i) {
+                        console.log('Enviado com sucesso');
+                    },
+                    error: function() {
+                        console.log('ERRO')
+                    },
+                })
+
+            });
+        }
     </script>
 </body>
 
