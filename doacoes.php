9748afaea44795f1524d2561cbb098372dfc47e9
ChavesArt <116437988+ChavesArt@users.noreply.github.com>
2024-09-25 16:01:09 -0300

    estilizando o index

--- doacoes.php
+++ doacoes.php
@@ -1,48 +1,37 @@
 <div id="donation-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
 
-                        <h2 class = "main-title">doações</h2>
-                    </div>
+    <div class="container ">
+        <div class="row">
 
-                    <div class="col-md-6 contact-box">
-                                <!--DOAR-->
-            <div class="doar-receber">
-                <div class="envia-form">
-                    <div class="esq-form-envia">
-                        <h1>Quer doar alguma coisa?</h1>
-                        <div>
-                            <img src="img/doar.svg" alt="pessoa doando um alimento">
-                            <p>Sua doação pode ajudar muitas pessoas</p>
-                        </div>
-                    </div>
-                    <div class="dir-form-envia">
-                        <a class="btn-index" href="formdoar.php">Doar</a>
-                    </div>
-                </div>
+            <div class="col-12  text-center my-1">
+                <h1 class=" main-title">Seja um doador</h1>
+            </div>
 
-                    </div>
-                    </DIV>
+            <div class="col-6 ">
+                <img class="img-fluid" src="img/doacao.svg" alt="doação de  alimento">
+            </div>
 
-                    <div class="col-md-6 contact-box">
-
-                    <div class="envia-form">
-                    <div class="esq-form-envia">
-                        <h1>Quer receber doações?</h1>
-                        <div>
-                            <img src="img/receber.svg" alt="recepção alimento">
-                            <p>Clique no botão se quiser receber alimentos</p>
-                        </div>
-                    </div>
-                    <div class="dir-form-envia">
-                        <a class="btn-index" href="receber.php">Receber</a>
-                    </div>
-                </div>
-                    
-                    </div>
-
-                </div>
-
+            <div class="col-6 justify-content text-center">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum consequuntur tenetur suscipit quo cumque maxime provident amet totam! Maxime nobis provident similique neque at facere incidunt odit doloribus blanditiis assumenda!</p>
+                <a class="btn-index" href="formdoar.php">Doar</a>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-12 text-center my-1">
+                <h1 class= "main-title">Precisa de doação</h1>
+            </div>
+
+            <div class="col-6">
+                <img src="img/receber.svg" alt="recepção de alimento">
+            </div>
+
+            <div class="col-6 justify-contnter text-center">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit consequuntur suscipit tenetur necessitatibus perferendis ea molestias perspiciatis. Enim fuga architecto facilis! Quo, nihil. Sapiente nemo repellat minima, quod enim nihil?Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia cumque impedit illum temporibus odio debitis repudiandae necessitatibus recusandae cum et! Autem voluptas porro ipsum odio repellat nisi libero est dolore!</p>
+            <a class = "btn-index" href="receber.php">Receber</a>
+            </div>
+        </div>
+
+</div>
+
+</div>
